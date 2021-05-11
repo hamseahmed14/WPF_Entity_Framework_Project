99cd43accd9addd3c03496a4bf76b56313430e56
Hamse Ahmed <hamse2727@gmail.com>
2021-05-11 14:29:23 +0100

    Add Create member method

---
+++ LibraryApp/LibraryBusiness/MemberCRUDManager.cs
@@ -0,0 +1,35 @@
+﻿using LibraryApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBusiness
+{
+    public class MemberCRUDManager
+    {
+        public Member SelectedMember { get; set; }
+
+        public void CreateMember(string firstname, string lastname, string email, string phonenumber, string housenumber, string street, string city, string postalcode)
+        {
+            var newMember = new Member() {FirstName = firstname, LastName = lastname, Email = email, PhoneNumber = phonenumber, HouseNumber = housenumber,Street = street, City = city, PostalCode = postalcode, Role ='M'};
+            AddMember(newMember);
+        }
+
+        public void CreateMember(string firstname, string lastname, string email, string housenumber, string street, string city, string postalcode)
+        {
+            var newMember = new Member() { FirstName = firstname, LastName = lastname, Email = email, HouseNumber = housenumber, Street = street, City = city, PostalCode = postalcode, Role = 'M' };
+            AddMember(newMember);
+        }
+
+        private void AddMember(Member member)
+        {
+            using (var db = new LibraryContext())
+            {
+                db.Members.Add(member);
+                db.SaveChanges();
+            }
+        }
+    }
+}
--- LibraryApp/LibraryBusiness/Program.cs
+++ LibraryApp/LibraryBusiness/Program.cs
@@ -6,7 +6,10 @@ namespace LibraryBusiness
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var m = new MemberCRUDManager();
+
+            m.CreateMember("Hamse","Ahmed","hamse-27@hotmail.com","21A","London Road","leicester","LE8 0SU");
         }
     }
 }
