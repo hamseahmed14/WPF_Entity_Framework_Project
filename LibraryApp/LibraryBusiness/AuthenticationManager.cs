3379dafb42eebb00c2decef39a7eceea144222ed
Hamse Ahmed <hamse2727@gmail.com>
2021-05-11 21:40:22 +0100

    Refractor business methods

---
+++ LibraryApp/LibraryBusiness/AuthenticationManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryApp;
+
+namespace LibraryBusiness
+{
+    public class AuthenticationManager
+    {
+        Crypto enc = new Crypto();
+
+        public string AuthIsCorrect(string username, string password)
+        {
+            
+            using (var db = new LibraryContext())
+            {
+                var selectedMember = db.Members.Where(c => c.Username == username && c.Password == password).ToList();
+                if(selectedMember.Count != 0)
+                {
+                    foreach (var mem in selectedMember)
+                    {
+                        return mem.Username;
+                    }
+                    
+                }
+                return "Incorrect Credentials";
+            }
+        }
+
+
+        public char GetRole(string cred)
+        {
+            using (var db = new LibraryContext())
+            {
+                var role = db.Members.Where(c => c.Username == cred).ToList();
+
+                foreach (var item in role)
+                {
+                    return item.Role;
+                }
+
+                return ' ';
+            }
+        }
+    }
+}
--- LibraryApp/LibraryBusiness/MemberCRUDManager.cs
+++ LibraryApp/LibraryBusiness/MemberCRUDManager.cs
@@ -12,16 +12,16 @@ namespace LibraryBusiness
     {
         public Member SelectedMember { get; set; }
 
-        public void CreateMember(string firstname, string lastname, string email, string phonenumber, string housenumber, string street, string city, string postalcode, string password)
+        public void CreateMember(string firstname, string lastname,string username , string email, string phonenumber, string housenumber, string street, string city, string postalcode, string password)
         {
-            var newMember = new Member() {FirstName = firstname, LastName = lastname, Email = email, PhoneNumber = phonenumber, HouseNumber = housenumber,Street = street, City = city, PostalCode = postalcode, Role ='M', Password = password};
+            var newMember = new Member() {FirstName = firstname, LastName = lastname,Username = username, Email = email, PhoneNumber = phonenumber, HouseNumber = housenumber,Street = street, City = city, PostalCode = postalcode, Role ='M', Password = password};
             AddMember(newMember);
         }
 
-        public void CreateMember(string firstname, string lastname, string email, string housenumber, string street, string city, string postalcode, string password)
+        public void CreateMember(string firstname, string lastname,string username ,string email, string housenumber, string street, string city, string postalcode, string password)
         {
             
-            var newMember = new Member() { FirstName = firstname, LastName = lastname, Email = email, HouseNumber = housenumber, Street = street, City = city, PostalCode = postalcode, Role = 'M', Password = password};
+            var newMember = new Member() { FirstName = firstname, LastName = lastname,Username = username ,Email = email, HouseNumber = housenumber, Street = street, City = city, PostalCode = postalcode, Role = 'M', Password = password};
             AddMember(newMember);
         }
 
