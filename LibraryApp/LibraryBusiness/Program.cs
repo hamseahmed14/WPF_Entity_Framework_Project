63c9067319679f2ba2cde9e4c739a19f4187ed03
Hamse Ahmed <hamse2727@gmail.com>
2021-05-11 13:29:46 +0100

    Add Business layer to project

---
+++ LibraryApp/LibraryBusiness/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryBusiness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
