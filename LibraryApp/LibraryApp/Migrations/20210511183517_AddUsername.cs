05d7ee74493b7eb47ef0b1c514c0db3a7be752d6
Hamse Ahmed <hamse2727@gmail.com>
2021-05-11 21:39:58 +0100

    Add username to database

---
+++ LibraryApp/LibraryApp/Migrations/20210511183517_AddUsername.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryApp.Migrations
+{
+    public partial class AddUsername : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Members",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Members");
+        }
+    }
+}
